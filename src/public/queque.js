ce7dc0b56577794463286eb82e99bb30b360b48e
cxrdonx <jose.eduardo.cardona@gmail.com>
2021-03-19 02:04:03 -0600

    master

--- src/public/queque.js
+++ src/public/queque.js
@@ -4,6 +4,7 @@ var push = document.querySelector("#push");
 var ingresar = document.querySelector("#ok");
 // var genOt = document.querySelector("#re");
    var canvas = document.querySelector("#canvas");
+   var secondCanvas = document.querySelector("#canvas2");
    var number = 240;
    var arr = [];
 
@@ -19,29 +20,50 @@ ingresar.addEventListener('click', function(){
     cleanLast();
 });
 */
+var x = 0;
+var y =0;
+
+var drawInCanvas = function(x, y, number){
+   this.x = x;
+   this.y = y;
+
+this.draw = function(){
+    ctx.fillText(number,this.x,this.y);
+}
+}
+
      function generate(){
          number++;
-         var ctx = canvas.getContext('2d');
          kill();
-         ctx.font = "40pt verdana";
-         ctx.fillStyle = "white";
-         ctx.lineWidth = '5';
-         ctx.fillText(number,100,78);
-         ctx.fillColor = "white";
+         var ctx = canvas.getContext('2d');
+       ctx.font = "40pt verdana";
+        ctx.fillStyle = "white";
+        ctx.lineWidth = '5';
+        ctx.fillText(number,100,78);
+         
+   
+       
      }
 
 function newTicket(){
       arr.push(number);
       var prueba =  Array.prototype.includes(arr);
-      console.log(prueba);
       console.log(arr);
+      seeQ();
   }
 
 function cleanLast(){
    kill();
     arr.pop();
     console.log(arr);
+   
 }
+
+function seeQ(){
+document.getElementById("textArea").value = arr ;
+}
+
+
 function kill(){
     canvas.width = 300;
     canvas.height = 150;
